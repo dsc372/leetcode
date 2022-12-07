70b0c45e39f98cfa40f31d82685a9ced7c9dcebd
DSC <2586900641@qq.com>
2022-12-07 17:23:34 +0800

    22.12.07

---
+++ DynamicProgramming/213打家劫舍2.js
@@ -0,0 +1,25 @@
+const readline=require('readline')
+const rl=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout,
+})
+
+const rob = function(nums) {
+    if(nums.length===1)return nums[0]
+    const dp=new Array(nums.length-1).fill(0)
+    dp[0]=nums[0]
+    dp[1]=Math.max(dp[0],nums[1])
+    for(let i=2;i<nums.length;i++){
+        dp[i]=Math.max(dp[i-2]+nums[i],dp[i-1])
+    }
+    console.log(dp)
+    return Math.max(dp[nums.length-1])
+}
+
+rl.on('line',function(line){
+    console.log(rob(line.split(' ').map(Number)))
+    rl.close()
+})
+rl.on('close',function(){
+    process.exit()
+})
