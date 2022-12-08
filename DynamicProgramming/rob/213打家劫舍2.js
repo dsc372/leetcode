65086760bc2495db6fcac92cd59fcb6d7a9bde51
DSC <2586900641@qq.com>
2022-12-08 16:47:10 +0800

    22.12.08

--- DynamicProgramming/rob/213打家劫舍2.js
+++ DynamicProgramming/rob/213打家劫舍2.js
@@ -4,6 +4,10 @@ const rl=readline.createInterface({
     output:process.stdout,
 })
 
+const rob2=function(nums){
+    if(nums.length===1)return nums[0]
+    return Math.max(rob(nums.slice(1)),rob(nums.slice(0,-1)))
+}
 const rob = function(nums) {
     if(nums.length===1)return nums[0]
     const dp=new Array(nums.length-1).fill(0)
@@ -12,12 +16,11 @@ const rob = function(nums) {
     for(let i=2;i<nums.length;i++){
         dp[i]=Math.max(dp[i-2]+nums[i],dp[i-1])
     }
-    console.log(dp)
     return Math.max(dp[nums.length-1])
 }
 
 rl.on('line',function(line){
-    console.log(rob(line.split(' ').map(Number)))
+    console.log(rob2(line.split(' ').map(Number)))
     rl.close()
 })
 rl.on('close',function(){
