4a80417c50af10949ce9ac655b833379f4859d64
DSC <2586900641@qq.com>
2022-12-29 16:50:28 +0800

    12.22.28

---
+++ DynamicProgramming/subsequence/647回文子串.js
@@ -0,0 +1,38 @@
+const readline =require('readline')
+const rl=readline.createInterface({
+    input:process.stdin,
+    output:process.stdout,
+})
+
+const countSubstrings = function(s) {
+    const dp=new Array(s.length).fill(0).map(item=>new Array(s.length).fill(0))
+    let res=0
+    for(let i=0;i<s.length;i++){
+        for(let j=0;j<=i;j++){
+            if(s[i]===s[j]){
+                if(i-1>j){
+                    if(dp[i-1][j+1]){
+                       dp[i][j]=true
+                       res++ 
+                    }else{
+                        dp[i][j]=false
+                    }
+                }else{
+                    dp[i][j]=true
+                    res++
+                }
+            }else{
+                dp[i][j]=false
+            }
+        }
+    }
+    return res
+};
+
+rl.on('line',function(line){
+  console.log(countSubstrings(line))
+  rl.close()  
+})
+rl.on('close',function(){
+    process.exit()
+})
